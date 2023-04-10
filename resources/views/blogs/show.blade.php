d508ad9bed24cba284a2a9b1cfe46ad7064cd5fd
Chamara Senarath <senarath.chamara@gmail.com>
2023-04-10 10:05:44 +0100

    Allow modifying blogs only for their creators

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -2,21 +2,22 @@
     <div class="flex flex-col px-12 md:px-24 lg:px-64 py-8">
         <div class="flex justify-between items-center">
             <h1 class="text-3xl md:text-7xl font-semibold">{{ $blog->title }}</h1>
-            <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                    </svg>
-                </label>
-                <ul tabindex="0"
-                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Edit blog</a></li>
-                    <li><a><label for="delete-blog-modal">Delete blog</label></a></li>
-
-                </ul>
-            </div>
+            @if ($blog->user_id === auth()->user()->id)
+                <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                        </svg>
+                    </label>
+                    <ul tabindex="0"
+                        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Edit blog</a></li>
+                        <li><a><label for="delete-blog-modal">Delete blog</label></a></li>
+                    </ul>
+                </div>
+            @endif
 
         </div>
         <p>{{ $blog->user->name }}</p>
